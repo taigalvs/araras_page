ee0737e7b73f8c5c5710a4614fafd4aa52605699
Taina Almeida <almeida_taina@hotmail.com>
2024-01-16 08:09:58 -0300

    feat: create footer and navbar components

---
+++ constants/index.ts
@@ -0,0 +1,76 @@
+// NAVIGATION
+export const NAV_LINKS = [
+  { href: "/", key: "home", label: "Inicio" },
+  { href: "/sobre", key: "about_us", label: "Sobre nós" },
+  { href: "/oque-fazemos", key: "what_we_do", label: "O que fazemos?" },
+  { href: "/midia", key: "media ", label: "Na mídia" },
+  { href: "/contato", key: "contact_us", label: "Contato" },
+];
+
+// FOOTER SECTION
+export const FOOTER_LINKS = [
+  {
+    label: "Inicio",
+    items: [
+      {
+        href: "javascript:void()",
+        name: "Inicio",
+      },
+      {
+        href: "javascript:void()",
+        name: "Sobre nós",
+      },
+      {
+        href: "javascript:void()",
+        name: "Time",
+      },
+      {
+        href: "javascript:void()",
+        name: "O que fazemos",
+      },
+    ],
+  },
+  {
+    label: "Mais",
+    items: [
+      {
+        href: "javascript:void()",
+        name: "Projetos",
+      },
+      {
+        href: "javascript:void()",
+        name: "Eventos",
+      },
+      {
+        href: "javascript:void()",
+        name: "Faça sua doação!",
+      },
+      {
+        href: "javascript:void()",
+        name: "Blog",
+      },
+    ],
+  },
+  {
+    label: "Conecte-se!",
+
+    items: [
+      {
+        href: "javascript:void()",
+        name: "Facebook",
+      },
+      {
+        href: "javascript:void()",
+        name: "Instagram",
+      },
+      {
+        href: "javascript:void()",
+        name: "Twitter",
+      },
+      {
+        href: "javascript:void()",
+        name: "Linkedin",
+      },
+    ],
+  },
+];
